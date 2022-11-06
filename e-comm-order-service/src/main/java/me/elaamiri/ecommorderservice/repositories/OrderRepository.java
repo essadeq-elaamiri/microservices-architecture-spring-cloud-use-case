3dfacc4942f2b71c5c482087055ec5c53f7bf1f1
essadeq-elaamiri <essadeq0701@gmail.com>
2022-11-06 23:22:44 +0100

    Order, repos

---
+++ e-comm-order-service/src/main/java/me/elaamiri/ecommorderservice/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package me.elaamiri.ecommorderservice.repositories;
+
+import me.elaamiri.ecommorderservice.entities.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
