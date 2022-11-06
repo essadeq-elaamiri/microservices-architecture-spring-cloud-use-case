344c7d06c5a22e65754054792649bba9f2fab16d
essadeq-elaamiri <essadeq0701@gmail.com>
2022-11-06 14:38:17 +0100

    Customer tested done

--- e-comm-customer-service/src/main/java/me/elaamiri/ecommcustomerservice/ECommCustomerServiceApplication.java
+++ e-comm-customer-service/src/main/java/me/elaamiri/ecommcustomerservice/ECommCustomerServiceApplication.java
@@ -1,7 +1,13 @@
 package me.elaamiri.ecommcustomerservice;
 
+import me.elaamiri.ecommcustomerservice.entities.Customer;
+import me.elaamiri.ecommcustomerservice.repositories.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class ECommCustomerServiceApplication {
@@ -10,4 +16,16 @@ public class ECommCustomerServiceApplication {
 		SpringApplication.run(ECommCustomerServiceApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner start(CustomerRepository customerRepository){
+		return args -> {
+			List.of("Essadeq", "hamza", "soukaina").forEach(s -> {
+				Customer customer = Customer.builder()
+						.name(s)
+						.email(String.format("%s@gmail.com", s))
+						.build();
+				customerRepository.save(customer);
+			});
+		};
+	}
 }
