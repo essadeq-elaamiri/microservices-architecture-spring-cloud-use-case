15e6f533adf8b4e1c6525cc44350d9ec2c035fe2
essadeq-elaamiri <essadeq0701@gmail.com>
2022-11-06 21:53:09 +0100

    init inventory repo

--- e-comm-inventory-service/src/main/java/me/elaamiri/ecomminventoryservice/repositories/ProductRepository.java
+++ e-comm-inventory-service/src/main/java/me/elaamiri/ecomminventoryservice/repositories/ProductRepository.java
@@ -1,4 +1,9 @@
 package me.elaamiri.ecomminventoryservice.repositories;
 
-public interface ProductRepository {
+import me.elaamiri.ecomminventoryservice.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface ProductRepository extends JpaRepository<Product, Long> {
 }
