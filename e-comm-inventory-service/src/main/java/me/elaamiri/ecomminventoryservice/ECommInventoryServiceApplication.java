80c78b8955cda946d7096298ccf4cd836ea9003d
essadeq-elaamiri <essadeq0701@gmail.com>
2022-11-06 22:03:09 +0100

    inventory test data + h2 config

--- e-comm-inventory-service/src/main/java/me/elaamiri/ecomminventoryservice/ECommInventoryServiceApplication.java
+++ e-comm-inventory-service/src/main/java/me/elaamiri/ecomminventoryservice/ECommInventoryServiceApplication.java
@@ -1,7 +1,14 @@
 package me.elaamiri.ecomminventoryservice;
 
+import me.elaamiri.ecomminventoryservice.entities.Product;
+import me.elaamiri.ecomminventoryservice.repositories.ProductRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
+import java.util.Random;
 
 @SpringBootApplication
 public class ECommInventoryServiceApplication {
@@ -10,4 +17,17 @@ public class ECommInventoryServiceApplication {
 		SpringApplication.run(ECommInventoryServiceApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner start(ProductRepository productRepository){
+		return args -> {
+			List.of("IMACX15", "Lenovo X14", "Infinix142", "R74", "XLa77").forEach(s -> {
+				Product product = Product.builder()
+						.name(s)
+						.price((new Random()).nextDouble(500, 5000))
+						.quantity((new Random()).nextInt(12,55))
+						.build();
+				productRepository.save(product);
+			});
+		};
+	}
 }
