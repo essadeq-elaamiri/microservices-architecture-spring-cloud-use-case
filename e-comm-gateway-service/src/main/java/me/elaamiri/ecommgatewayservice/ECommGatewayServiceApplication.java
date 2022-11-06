0b1d2565f82648c35e5b2742b5d28336e4da82f3
essadeq-elaamiri <essadeq0701@gmail.com>
2022-11-06 16:26:43 +0100

    gateway done

--- e-comm-gateway-service/src/main/java/me/elaamiri/ecommgatewayservice/ECommGatewayServiceApplication.java
+++ e-comm-gateway-service/src/main/java/me/elaamiri/ecommgatewayservice/ECommGatewayServiceApplication.java
@@ -2,12 +2,21 @@ package me.elaamiri.ecommgatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+
 public class ECommGatewayServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ECommGatewayServiceApplication.class, args);
 	}
-
+	@Bean
+	DiscoveryClientRouteDefinitionLocator dynamicRouting(ReactiveDiscoveryClient reactiveDiscoveryClient,
+														 DiscoveryLocatorProperties discoveryLocatorProperties){
+		return new DiscoveryClientRouteDefinitionLocator(reactiveDiscoveryClient, discoveryLocatorProperties);
+	}
 }
