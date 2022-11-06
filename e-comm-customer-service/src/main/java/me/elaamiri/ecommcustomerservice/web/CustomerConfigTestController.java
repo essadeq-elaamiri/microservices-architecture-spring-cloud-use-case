2c5bf01af972748a4e5ed704452aa3d964d34e3d
essadeq-elaamiri <essadeq0701@gmail.com>
2022-11-06 13:59:52 +0100

    access config params tests / refresh with actuator

--- e-comm-customer-service/src/main/java/me/elaamiri/ecommcustomerservice/web/CustomerConfigTestController.java
+++ e-comm-customer-service/src/main/java/me/elaamiri/ecommcustomerservice/web/CustomerConfigTestController.java
@@ -1,16 +1,18 @@
 package me.elaamiri.ecommcustomerservice.web;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
 @RestController
+@RefreshScope
 public class CustomerConfigTestController {
     // Injecting the configuration values
-    //@Value("${cusomer.params.c1}")
-    private String c1="val"; // dev param
+    @Value("${customer.params.c1}")
+    private String c1; // dev param
     @Value("${global.parmas.globlaName}")
     private String globalName; // dev param
 
