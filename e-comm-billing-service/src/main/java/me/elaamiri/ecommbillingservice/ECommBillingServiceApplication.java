e0ddfc0b0e4e8a020c73f2876e5bae8a95ddcaf8
essadeq-elaamiri <essadeq0701@gmail.com>
2022-11-12 23:47:23 +0100

    vault from Spring ++

--- e-comm-billing-service/src/main/java/me/elaamiri/ecommbillingservice/ECommBillingServiceApplication.java
+++ e-comm-billing-service/src/main/java/me/elaamiri/ecommbillingservice/ECommBillingServiceApplication.java
@@ -1,7 +1,14 @@
 package me.elaamiri.ecommbillingservice;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.vault.core.VaultTemplate;
+import org.springframework.vault.support.Versioned;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 public class ECommBillingServiceApplication {
@@ -10,4 +17,25 @@ public class ECommBillingServiceApplication {
 		SpringApplication.run(ECommBillingServiceApplication.class, args);
 	}
 
+
+	@Bean
+	CommandLineRunner start(VaultTemplate vaultTemplate){
+		return args -> {
+			// Write a secret
+			Map<String, String> data = new HashMap<>();
+			data.put("password", "Hashi123");
+			vaultTemplate.opsForVersionedKeyValue("secret")
+					.put("billing-service", data);
+			// Read a secret
+			Versioned<Map<String, Object>> readSecrets = vaultTemplate
+					.opsForVersionedKeyValue("secrets")
+					.get("billing-service");
+
+			if (readSecrets != null && readSecrets.hasData()){
+				System.out.println(readSecrets.getVersion());
+				System.out.println(readSecrets.getData().get("password"));
+			}
+
+		};
+	}
 }
