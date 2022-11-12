15b28fa6502fee845532e5b54de46ba7085d68af
essadeq-elaamiri <essadeq0701@gmail.com>
2022-11-12 16:16:24 +0100

    Read and consume Consul Config in our service. changing value

--- e-comm-billing-service/src/main/java/me/elaamiri/ecommbillingservice/ConsulConfigRestController.java
+++ e-comm-billing-service/src/main/java/me/elaamiri/ecommbillingservice/ConsulConfigRestController.java
@@ -1,12 +1,14 @@
 package me.elaamiri.ecommbillingservice;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
 @RestController
+@RefreshScope
 public class ConsulConfigRestController {
     // Inject the configuration value in a variable.
     // the same name as that in config service
