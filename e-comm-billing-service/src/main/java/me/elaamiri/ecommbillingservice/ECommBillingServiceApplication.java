ce1c9a75de455bc70d5fbaec160466732c7895d3
essadeq-elaamiri <essadeq0701@gmail.com>
2022-11-12 15:30:06 +0100

    billing service

---
+++ e-comm-billing-service/src/main/java/me/elaamiri/ecommbillingservice/ECommBillingServiceApplication.java
@@ -0,0 +1,13 @@
+package me.elaamiri.ecommbillingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommBillingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommBillingServiceApplication.class, args);
+	}
+
+}
